609325b67420f6bb2cdcbbb732ad29d15488b58d
simoncla <iam@simonclavey.co.uk>
2015-02-17 15:21:07 +0000

    Changing formatting.

---
+++ javascripts/app.js
@@ -0,0 +1,17 @@
+$('#mobile').click(function(e) {
+    e.preventDefault();
+  $('#phantom').attr('width', '460em');
+  // $('html').attr('style', 'max-width: 480px; margin: 0 auto;');
+});
+
+$('#tablet').click(function(e) {
+    e.preventDefault();
+  $('#phantom').attr('width', '768px');
+  // $('html').attr('style', 'max-width: 768px; margin: 0 auto;');
+});
+
+$('#desktop').click(function(e) {
+    e.preventDefault();
+  $('#phantom').attr('width', '1280px');
+  // $('html').attr('style', 'max-width: auto; margin: 0 auto;');
+});
